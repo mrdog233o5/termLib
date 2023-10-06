8452a61f7656ea662a7cec985edcdca6dcd7bbea
mrdog233o5 <mr.dog233o5@gmail.com>
2023-10-06 17:30:47 +0800

    commit

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import time
-
-class terminator():
+import sys
+import os
+class terminal():
     def __init__(self, color=[-1,-1,-1]):
         self.color = color
-    
     def print(self, text):
         print(text, end="")
-    
     def printls(self, text):
         print(text)
-    
     def cprint(self, text, color):
         print(f"\033[38;2;{color[0]};{color[1]};{color[2]}m{str(text)}\x1b[0m", end="")
-
-    def aprint(self, message, WaitTime=0.02):
+    def aprint(self, message, WaitTime=0.02, end="\n"):
         for i in message:
             self.cprint(i, color=self.color)
             time.sleep(WaitTime)
-
-    def printGrid(self, grid, space=10, revert=False):
+        print(end, end="")
+    def printgrid(self, grid, space=10, revert=False):
         if revert:
             grid = list(zip(*grid[::-1]))
             for i in range(len(grid)):
@@ -29,3 +26,9 @@ class terminator():
                 for i in range(space - len(element)):
                     self.cprint(" ", color=self.color)
             print("")
+    def cleanln(self):
+        sys.stdout.write('\r\33[K')
+        sys.stdout.flush()
+
+    def clean(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
