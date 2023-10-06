420c72f58c04622971c66b5167a1be6f9cb9b5b4
mrdog233o5 <mr.dog233o5@gmail.com>
2023-10-06 18:44:52 +0800

    fixed lvim thing

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import time
 import sys
 import os
-class printer():
+
+class ansiConverter:
+    def __init__(self):
+        self.RESET = "\33[0m]"
+        self.RESET = self.RESET.rstrip(self.RESET[-1])
+
+    def rgb_ansi(self, text, rgb=[255,255,255]):
+        r,g,b = rgb[0],rgb[1],rgb[2]
+        ret = f"\033[38;2;{r};{g};{b}m{str(text)}]"
+        return ret.rstrip(ret[-1])
+
+class printer:
     def __init__(self, color=[-1,-1,-1]):
         self.color = color
-    def print(self, text):
-        print(text, end="")
-    def printls(self, text):
-        print(text)
-    def cprint(self, text, color):
-        print(f"\033[38;2;{color[0]};{color[1]};{color[2]}m{str(text)}\x1b[0m", end="")
-    def aprint(self, message, WaitTime=0.02, end="\n"):
+
+    def cprint(self, msg, color):
+        tempClass = ansiConverter()
+        print(tempClass.rgb_ansi(msg, rgb=color), end="")
+
+    def aprint(self, message, WaitTime=0.015, end="\n"):
         for i in message:
             self.cprint(i, color=self.color)
             time.sleep(WaitTime)
@@ -26,9 +36,14 @@ class printer():
                 for i in range(space - len(element)):
                     self.cprint(" ", color=self.color)
             print("")
+
     def cleanln(self):
-        sys.stdout.write('\r\33[K')
+        temp = '\r\33[K]'
+        sys.stdout.write(temp.strip(temp[-1]))
         sys.stdout.flush()
 
     def clean(self):
         os.system('cls' if os.name == 'nt' else 'clear')
+
+a = printer((17, 196, 17))
+a.aprint("asdkfuhqweoaidskljfhoiqukljehasdflkujhqwef2891ui3oweqd")
