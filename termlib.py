44746cd538a1c923598782fe111448893918f69a
mrdog233o5 <mr.dog233o5@gmail.com>
2023-10-11 00:50:58 +0800

    commit

--- termlib.py
+++ termlib.py
@@ -1,7 +1,21 @@
 import time
 import sys
 import os
-from ansiConvert import *
+
+class ansiConverter:
+    def __init__(self):
+        self.RESET = "\33[0m]"
+        self.RESET = self.RESET.rstrip(self.RESET[-1])
+
+    def ansiText(self, text, rgb=(255,255,255)):
+        r,g,b = rgb
+        ret = f"\033[38;2;{r};{g};{b}m{str(text)}]"
+        return ret.rstrip(ret[-1])+self.RESET
+
+    def ansiBlock(self, rgb=(255,255,255)):
+        r,g,b = rgb
+        ret = f"\033[48;2;{r};{g};{b}m ]"
+        return ret.rstrip(ret[-1])+self.RESET
 
 class printer:
     def __init__(self, color=[-1,-1,-1]):
